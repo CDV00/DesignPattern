1b7f9d3bfed7e3049fa8f560f3677caf127e5fda
vucd <vucd@vietinfo.tech>
2024-04-08 13:43:22 +0700

    commit Singleton

---
+++ Singleton/UserManager.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton {
+    public interface IUserManager {
+       public string[] Usernames { get;}
+    }
+    public class UserManager : IUserManager {
+        private readonly string[] usernames;
+        private UserManager() {
+            //We can make a DB/ distributed cache call to get initial user information
+            usernames = new[] { "User1", "User2" };
+        }
+        //private static readonly Lazy<UserManager> _instance = new Lazy<UserManager>(() => new UserManager());
+    
+        //public static UserManager Instance => _instance.Value;
+
+        public string[] Usernames => usernames;
+
+    }
+}
---
+++ Singleton/Userprocessor.cs
@@ -0,0 +1,16 @@
+﻿namespace Singleton {
+    public class Userprocessor {
+        private readonly IUserManager userManager;
+
+        public Userprocessor(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public string Process() {
+            var users = userManager.Usernames;
+            //We can implement some log here
+            return users.FirstOrDefault() ?? string.Empty;
+        }
+    }
+}
